48a3714ff2262ba07dd1fc75a2d80c9713155ac1
Adrian Zienkiewicz <zienkiewiczadrian1@gmail.com>
2025-05-04 13:58:18 +0200

    Added templates

--- Projekt2/BinaryHeapPriorityQueue.h
+++ Projekt2/BinaryHeapPriorityQueue.h
@@ -2,107 +2,114 @@
 #include <iostream>
 #include <stdexcept>
 #include "DynamicArray.h"
-struct Element {
-	int value;
-	int priority;
 
-	friend std::ostream& operator<<(std::ostream& os, const Element& element) {
-		os << "Value: " << element.value << ", Priority: " << element.priority;
-		return os;
-	}
-};
-
-class PriorityQueue {
+template <typename T>
+class HeapPriorityQueue {
 private:
-	DynamicArray<Element> heap;
+    struct Element {
+        T value;
+        int priority;
 
-	int parent(int index) {
-		return (index - 1) / 2;
-	}
-	int leftChild(int index) {
-		return 2 * index + 1;
-	}
-	int rightChild(int index) {
-		return 2 * index + 2;
-	}
-	void swap(int index1, int index2) {
-		Element temp = heap.get(index1);
-		heap.get(index1) = heap.get(index2);
-		heap.get(index2) = temp;
-	}
-	void heapifyUp(int index) {
-		while (index > 0 && heap.get(index).priority > heap.get(parent(index)).priority) {
-			swap(index, parent(index));
-			index = parent(index);
-		}
-	}
-	void heapifyDown(int index)
-	{
-		int size = heap.getSize();
-		int largest = index;
-		int left = leftChild(index);
-		int right = rightChild(index);
-		if (left < size && heap.get(left).priority > heap.get(largest).priority) {
-			largest = left;
-		}
-		if (right < size && heap.get(right).priority > heap.get(largest).priority) {
-			largest = right;
-		}
-		if (largest != index) {
-			swap(index, largest);
-			heapifyDown(largest);
-		}
-	}
+        Element() : value(T()), priority(0) {}
+
+        Element(const T& val, int prio) : value(val), priority(prio) {}
+
+        friend std::ostream& operator<<(std::ostream& os, const Element& element) {
+            os << "Data: " << element.value << ", Priority: " << element.priority;
+            return os;
+        }
+    };
+
+    DynamicArray<Element> heap;
+
+    int parent(int index) const { return (index - 1) / 2; }
+    int leftChild(int index) const { return 2 * index + 1; }
+    int rightChild(int index) const { return 2 * index + 2; }
+
+    void swap(int index1, int index2) {
+        Element temp = heap.get(index1);
+        heap.get(index1) = heap.get(index2);
+        heap.get(index2) = temp;
+    }
+
+    void heapifyUp(int index) {
+        while (index > 0 && heap.get(index).priority > heap.get(parent(index)).priority) {
+            swap(index, parent(index));
+            index = parent(index);
+        }
+    }
+
+    void heapifyDown(int index) {
+        int size = heap.getSize();
+        int largest = index;
+        int left = leftChild(index);
+        int right = rightChild(index);
+
+        if (left < size && heap.get(left).priority > heap.get(largest).priority) {
+            largest = left;
+        }
+        if (right < size && heap.get(right).priority > heap.get(largest).priority) {
+            largest = right;
+        }
+        if (largest != index) {
+            swap(index, largest);
+            heapifyDown(largest);
+        }
+    }
+
 public:
-	void enqueue(int value, int priority) {
-		Element newElement = { value, priority };
-		heap.addBack(newElement);
-		heapifyUp(heap.getSize() - 1);
-	}
-	void print() {
-		for (int i = 0; i < heap.getSize(); i++) {
-			std::cout << heap.get(i) << std::endl;
-		}
-	}
+    void enqueue(const T& value, int priority) {
+        Element newElement = { value, priority };
+        heap.addBack(newElement);
+        heapifyUp(heap.getSize() - 1);
+    }
 
-	
-	Element	dequeue() {
-		if (heap.getSize() == 0) {
-			throw std::out_of_range("Heap is empty");
-		}
-		Element root = heap.get(0);
-		heap.get(0) = heap.get(heap.getSize() - 1);
-		heap.removeBack();
-		heapifyDown(0);
-		return root;
-	}
-	
-	void set(int index, int newPriority)
-	{
-		if (index < 0 || index >= heap.getSize()) {
-			throw std::out_of_range("Index out of range");
-		}
-		int oldPriority = heap.get(index).priority;
-		heap.get(index).priority = newPriority;
-		if (newPriority > oldPriority) {
-			heapifyUp(index);
-		}
-		else {
-			heapifyDown(index);
-		}
+    void print() const {
+        for (int i = 0; i < heap.getSize(); i++) {
+            std::cout << heap.get(i) << std::endl;
+        }
+    }
 
+    T dequeue() {
+        if (empty()) {
+            throw std::out_of_range("Heap is empty");
+        }
+        Element root = heap.get(0);
+        heap.get(0) = heap.get(heap.getSize() - 1);
+        heap.removeBack();
+        if (heap.getSize() > 0) {
+            heapifyDown(0);
+        }
+        return root.value;
+    }
 
+    void changePriority(int index, int newPriority) {
+        if (index < 0 || index >= heap.getSize()) {
+            throw std::out_of_range("Index out of range");
+        }
+        int oldPriority = heap.get(index).priority;
+        heap.get(index).priority = newPriority;
 
-	}
+        if (newPriority > oldPriority) {
+            heapifyUp(index);
+        }
+        else {
+            heapifyDown(index);
+        }
+    }
 
-	Element peek() {
-		if (heap.getSize() == 0) {
-			throw std::out_of_range("Heap is empty");
-		}
-		return heap.get(0);
-	}
+    const Element& peek() const {
+        if (empty()) {
+            throw std::out_of_range("Heap is empty");
+        }
+        return heap.get(0);
+    }
 
-	int getSizePQ() {
-		return heap.getSize();
-	}
+    size_t size() const {
+        return heap.getSize();
+    }
+
+    bool empty() const {
+        return heap.getSize() == 0;
+    }
 };
--- Projekt2/LinkedListPriorityQueue.h
+++ Projekt2/LinkedListPriorityQueue.h
@@ -3,60 +3,70 @@
 #include <stdexcept>
 
 template <typename T>
-class LinkedList {
+class ListPriorityQueue {
 private:
-    struct Node {
-        T data;
-		int priority;
-        Node* next;
-        Node* prev;
-    
-        Node(const T& val, int prio) : data(val), priority(prio), prev(nullptr), next(nullptr) {}
+    struct Element {
+        T content;
+        int priority;
+
+        friend std::ostream& operator<<(std::ostream& os, const Element& element) {
+            os << "Content: " << element.content << ", Priority: " << element.priority;
+            return os;
+        }
     };
 
+    struct Node {
+        Element data;
+        Node* next;
+        Node* prev;
+
+        Node(Element ele) : data(ele), prev(nullptr), next(nullptr) {}
+    };
 
     Node* head;
     Node* tail;
     size_t size;
 
-
 public:
-    PriorityQueue() : head(nullptr), tail(nullptr), size(0) {}
+    ListPriorityQueue() : head(nullptr), tail(nullptr), size(0) {}
 
-
-
-
-    void enqueue(cont T& value, int priority)
-    {
-        Node* newNode = new Node(value, priority);
-        if (head == nullptr) {
-			head = newNode;
-			tail = newNode;
-			
+    ~ListPriorityQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
         }
-        else if (head->priority < priority)
-        {
+    }
+
+    void enqueue(const T& content, int priority) {
+        enqueue(Element{ content, priority });
+    }
+
+    void enqueue(Element ele) {
+        Node* newNode = new Node(ele);
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        }
+        else if (head->data.priority < ele.priority) {
             newNode->next = head;
             head->prev = newNode;
             head = newNode;
         }
-        else if (tail->priority > priority)
-        {
+        else if (tail->data.priority >= ele.priority) {
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
-        else
-        {
+        else {
             Node* current = head;
-            while (current != nullptr && current->priority >= priority)
-            {
+            while (current->next != nullptr && current->next->data.priority >= ele.priority) {
                 current = current->next;
             }
             newNode->next = current->next;
             newNode->prev = current;
-            if (current->next != nullptr)
-            {
+            if (current->next != nullptr) {
                 current->next->prev = newNode;
             }
             else {
@@ -67,20 +77,81 @@ public:
         size++;
     }
 
-    Node dequeue()
-    {
-        if (!head)
-        {
+    T dequeue() {
+        if (!head) {
             throw std::out_of_range("Queue is empty");
         }
 
+        Node* temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
+        else {
+            tail = nullptr;
+        }
 
-
-
+        T content = temp->data.content;
+        delete temp;
         size--;
-
-
+        return content;
     }
 
+    size_t getSize() const {
+        return size;
+    }
 
+    bool isEmpty() const {
+        return size == 0;
+    }
+
+    Element peek() const {
+        if (isEmpty()) {
+            throw std::out_of_range("Queue is empty");
+        }
+        return head->data;
+    }
+
+    void print() const {
+        if (!head) {
+            throw std::out_of_range("Queue is empty");
+        }
+
+        Node* current = head;
+        while (current) {
+            std::cout << current->data << std::endl;
+            current = current->next;
+        }
+    }
+
+    bool changePriority(const T& content, int oldPriority, int newPriority) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data.content == content && current->data.priority == oldPriority) {
+                T val = current->data.content;
+
+                if (current->prev != nullptr) {
+                    current->prev->next = current->next;
+                }
+                else {
+                    head = current->next;
+                }
+
+                if (current->next != nullptr) {
+                    current->next->prev = current->prev;
+                }
+                else {
+                    tail = current->prev;
+                }
+
+                delete current;
+                size--;
+
+                enqueue(val, newPriority);
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
 };
